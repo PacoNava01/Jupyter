eb3984321057dd61a79984902906eda97487386a
PacoNava01 <francisconavavalencia@ciencias.unam.mx>
2026-05-08 12:56:59 -0600

    Seguimiento beta 0.9

--- Tesis-Proyecto/src/main.py
+++ Tesis-Proyecto/src/main.py
@@ -5,12 +5,11 @@ import time
 import cv2
 import numpy as np
 
+
 # --- Clase PID robusta --- 
 class PID:
     def __init__(self,kP,kI,kD):
-        self.kP = kP
-        self.kI = kI
-        self.kD = kD
+        self.kP,self.kI,self.kD = kP,kI,kD
         self.last_error = 0
         self.integral = 0 
         self.last_time = time.time()
@@ -18,13 +17,17 @@ class PID:
     def update(self,error):
         now = time.time()
         dt = now - self.last_time
-        if dt  <= 0:
-            return 0
+        if dt  <= 0: return 0
+        
         #Proporcional
         P = self.kP * error 
+
         #Integral
         self.integral += error * dt
+        #Limitar la integral(antiwindup) para evitar los latigazos
+        self.integral= max(-10,min(10,self.integral))
         I = self.kI * self.integral
+        
         #Derivativo
         D = self.kD * (error - self.last_error) / dt
 
@@ -32,13 +35,14 @@ class PID:
         self.last_time = now
         return P + I + D
 
-# --- COnfiguracion ---
-detector = ObjectDetector("Jupyter/Tesis-Proyecto/data/model.pkl")
+# --- Configuracion de archivos ---
+detector = ObjectDetector("Jupyter/Tesis-Proyecto/data/model.pkl",
+                          "Jupyter/Tesis-Proyecto/data/scaler.pkl")
+
 cam = init_cam()
 
 # Servo (solo eje X)
 servo_x, _ = init_servos()
-servo_x_angle = 90  # centro inicial
 
 #constante de resoluciones y centro
 FRAME_W = 640
@@ -48,38 +52,42 @@ CENTER_X = FRAME_W // 2
 angle_x = 90.0 #Float para mayor presicion en el ajuste
 dead_zone = 5 #Banda muerta reducida gracias al PID
 
+#Timeout de inercia ayuda a que si el recall falla,el servo no se detenga
+last_detection_time = time.time()
+detection_timeout = 0.2 #segundos
+
 # --- Sincronización PID ----
 #kP: Reaccion inicial, kD: Amortigua el temblor, kI: Presicion final
-pid_x = PID(kP=0.1,kI=0.05,kD=0.6)
+#Valores departida [0.06,,0.02,0.0005]
+pid_x = PID(kP=0.01,kI=0.02,kD=0.0005)
 
 
 # --- Rangos HSV rojo ----
-low_red1 = np.array([0, 120, 90])
-up_red1  = np.array([10, 255, 255])
-
-low_red2 = np.array([170, 120, 90])
-up_red2  = np.array([180, 255, 255])
+low_red1,up_red1 = np.array([0, 120, 90]),np.array([10, 255, 255])
+low_red2, up_red2 = np.array([170, 120, 90]),np.array([180, 255, 255])
 
 
 print("Iniciando prueba en tiempo real...")
+Servo2Pos(servo_x, int(angle_x))
 cv2.namedWindow('Comparacion', cv2.WINDOW_AUTOSIZE)
-Servo2Pos(servo_x, servo_x_angle)
 
 while True:
 
     frame = cam.capture_array()
     frame = cv2.rotate(frame, cv2.ROTATE_180)
 
-    display_frame = frame.copy()
+    #1. Preprocesamiento de color (solo 1 vez)
+    hsv_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
 
-    # 1. DEetección y mascara HSV
-    mask1 = obtener_mask(frame, low_red1, up_red1)
-    mask2 = obtener_mask(frame, low_red2, up_red2)
+    #2. Deteccion con el nuevo detector (incluye escalado y solidez internamente)
+    mask1 = obtener_mask(hsv_frame, low_red1, up_red1)
+    mask2 = obtener_mask(hsv_frame, low_red2, up_red2)
     final_mask = detector.process_frame(frame, cv2.add(mask1,mask2))
    
-   # 2. Localización del centroide más grande
+   # 3. Localización del centroide más grande
     contours, _ = cv2.findContours(final_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     best_centroid = None
+
     if contours:
         c = max(contours,key = cv2.contourArea)
         if cv2.contourArea(c) > 600:
@@ -88,34 +96,35 @@ while True:
                 cx = int(M["m10"]/M["m00"])
                 cy = int(M["m01"]/M["m00"])
                 best_centroid = (cx,cy)
+                last_detection_time = time.time()
 
-    #3. Logica de control PID
-    if best_centroid:
-        error_x = CENTER_X - best_centroid[0] #Error relativo al centro 
+    #4. Logica de control PID
+    #Si detectctamos el objeto o si lo perdimos hace muy poco (inercia)
+    if best_centroid  or (time.time() - last_detection_time < detection_timeout):
+        
+        #Si best_centroid es None (falla el recall), usamos el ultimo error conocido
+        if best_centroid:
+            error_x = CENTER_X - best_centroid[0] #Error relativo al centro 
+        else:
+            error_x = pid_x.last_error #Mantener direccion
 
-        if abs(error_x) > dead_zone: #originalente 5
+        if abs(error_x) > dead_zone:
             #El PID calcula cuanto movernos, no a donde ir directamente
             adjustment = pid_x.update(error_x)
-            #print(adjustment)
-
             #Actualizamos la posicion actual (invertir el signo si el servo va al lado contrario)
             angle_x += adjustment
 
             #Limitar el rango fisico de movimiento
             angle_x = max(10,min(170,angle_x))
+            Servo2Pos(servo_x,angle_x)
             print(angle_x)
-
-            #acomodamos el angulo
-            Servo2Pos(servo_x,int(angle_x))
-
-        #Visualización
-        cv2.circle(display_frame,best_centroid,10,(0,255,0),2)
-
-
   
-    # ----------------------------
-    # VISUALIZACIÓN
-    # ----------------------------
+    #Visualizacion
+    display_frame = frame.copy()
+    if best_centroid:
+        cv2.circle(display_frame,best_centroid,10,(0,255,0),1)
+        cv2.putText(display_frame,"siguiendo",(10,30),1,1,(0,255,0),2)
+
     ML_mask_bgr = cv2.cvtColor(final_mask, cv2.COLOR_GRAY2BGR)
     combined = np.hstack((display_frame, ML_mask_bgr))
 
@@ -123,10 +132,7 @@ while True:
 
     if cv2.waitKey(1) & 0xFF == 13:
         break
-
-
-# ----------------------------
 # CIERRE
-# ----------------------------
+
 cam.stop()
 cv2.destroyAllWindows()
